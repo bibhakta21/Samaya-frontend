c917845bfe42b73c813512159d269b5837cd5623
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-07-04 17:49:03 +0545

    added secondary button

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -10,6 +10,13 @@ const Button = () => {
       >
         Primary
       </button>
+      <button
+        type="button"
+        className="inline-block rounded border-2 border-primary-100 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:border-primary-accent-200 hover:bg-secondary-50/50 focus:border-primary-accent-200 focus:bg-secondary-50/50 focus:outline-none focus:ring-0 active:border-primary-accent-200 motion-reduce:transition-none dark:border-primary-400 dark:text-primary-300 dark:hover:bg-blue-950 dark:focus:bg-blue-950"
+        data-twe-ripple-init
+      >
+        Secondary
+      </button>
      
   );
 };
