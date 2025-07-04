72bd14b5b071a29dc83f0ca9a43a548416f0cf47
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-07-04 17:50:29 +0545

    added info button

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -45,21 +45,7 @@ const Button = () => {
       >
         Info
       </button>
-      <button
-        type="button"
-        className="inline-block rounded border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-300 hover:text-neutral-200 focus:border-neutral-300 focus:text-neutral-200 focus:outline-none focus:ring-0 active:border-neutral-300 active:text-neutral-200 motion-reduce:transition-none dark:hover:bg-neutral-600 dark:focus:bg-neutral-600"
-        data-twe-ripple-init
-      >
-        Light
-      </button>
-      <button
-        type="button"
-        className="inline-block rounded border-2 border-neutral-800 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-800 transition duration-150 ease-in-out hover:border-neutral-800 hover:bg-neutral-100 hover:text-neutral-800 focus:border-neutral-800 focus:bg-neutral-100 focus:text-neutral-800 focus:outline-none focus:ring-0 active:border-neutral-900 active:text-neutral-900 motion-reduce:transition-none dark:text-neutral-600 dark:hover:bg-neutral-900 dark:focus:bg-neutral-900"
-        data-twe-ripple-init
-      >
-        Dark
-      </button>
-    </div>
+     
   );
 };
 
