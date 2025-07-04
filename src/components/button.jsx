5f4c1f6a3551d9d82d151ddad273e87a958c852c
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-07-04 17:48:39 +0545

    added primary button

---
+++ src/components/button.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div>
+      <button
+        type="button"
+        className="inline-block rounded border-2 border-primary px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:border-primary-accent-300 hover:bg-primary-50/50 hover:text-primary-accent-300 focus:border-primary-600 focus:bg-primary-50/50 focus:text-primary-600 focus:outline-none focus:ring-0 active:border-primary-700 active:text-primary-700 motion-reduce:transition-none dark:text-primary-500 dark:hover:bg-blue-950 dark:focus:bg-blue-950"
+        data-twe-ripple-init
+      >
+        Primary
+      </button>
+     
+  );
+};
+
+export default Button;
