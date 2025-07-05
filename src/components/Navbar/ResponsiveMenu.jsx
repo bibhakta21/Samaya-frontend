4748548bea3435ca784e75b90f5a8cdf14d08d55
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-07-05 17:16:30 +0545

    added nav item in responsive menu

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -12,7 +12,15 @@ const ResponsiveMenu = ({ isOpen }) => {
           transition={{ duration: 0.3 }}
           className="absolute top-20 left-0 w-full h-screen z-20 lg:hidden"
         >
-        
+          <div className="text-xl font-semibold uppercase bg-blue-600 text-white py-10 m-6 rounded-3xl">
+          <ul className="flex flex-col justify-center items-center gap-10">
+          <li>  <a href="/">Home </a></li>
+          <li> <a href="/">product </a></li>
+          <li> <a href="/">About </a></li>
+          <li> <a href="/">Contact </a></li>
+        </ul>
+
+          </div>
         </motion.div>
       )}
     </AnimatePresence>
